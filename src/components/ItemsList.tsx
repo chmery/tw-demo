13cf55c6621e265e683266e95180d89882b6d682
Wiktor Filip <karektv@gmail.com>
2024-03-17 19:51:41 +0100

    items list component

---
+++ src/components/ItemsList.tsx
@@ -0,0 +1,51 @@
+import { IoMdAdd } from "react-icons/io";
+import { Button } from "./Button";
+
+interface Item {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
+
+interface ItemProps {
+  onAdd: (id: number) => void;
+  item: Item;
+}
+
+interface ItemsListProps {
+  items: Item[];
+}
+
+const Item = ({ item, onAdd }: ItemProps) => {
+  const { id, title, thumbnailUrl } = item;
+
+  return (
+    <div className="flex gap-x-4 items-center border-solid border-b border-grey-light py-6 px-4 last">
+      <img className="w-20 h-20 rounded-lg" src={thumbnailUrl} alt={title} />
+      <div className="text-left">
+        <h3 className="font-medium">Item {id}</h3>
+        <p className="text-grey-dark">{title}</p>
+      </div>
+      <Button
+        className="ml-auto flex items-center gap-x-1"
+        onClick={() => onAdd(id)}
+      >
+        Add <IoMdAdd />
+      </Button>
+    </div>
+  );
+};
+
+export const ItemsList = ({ items }: ItemsListProps) => {
+  const addItemHandler = (id: number) => {};
+
+  return (
+    <div>
+      {items.map((item) => (
+        <Item item={item} key={item.id} onAdd={addItemHandler} />
+      ))}
+    </div>
+  );
+};
--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,4 +1,22 @@
 import { Button } from "../components/Button";
+import { ItemsList } from "../components/ItemsList";
+
+const TEST_DATA = [
+  {
+    albumId: 1,
+    id: 1,
+    title: "accusamus beatae ad facilis cum similique qui sunt",
+    url: "https://via.placeholder.com/600/92c952",
+    thumbnailUrl: "https://via.placeholder.com/150/92c952",
+  },
+  {
+    albumId: 1,
+    id: 2,
+    title: "reprehenderit est deserunt velit ipsam",
+    url: "https://via.placeholder.com/600/771796",
+    thumbnailUrl: "https://via.placeholder.com/150/771796",
+  },
+];
 
 export const Root = () => {
   return (
@@ -13,6 +31,7 @@ export const Root = () => {
         to Enhance Your Favorites Collection.
       </p>
       <Button className="w-[300px]">Start Searching</Button>
+      <ItemsList items={TEST_DATA} />
     </div>
   );
 };
