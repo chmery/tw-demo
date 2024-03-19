2e52442639703ebf77aafca35983c5fba9943b2f
Wiktor Filip <karektv@gmail.com>
2024-03-19 15:46:42 +0100

    cleanup

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,6 +1,4 @@
 import { Button } from "../components/Button";
-import { SearchBar } from "../components/SearchBar";
-import { Select } from "../components/Select/Select";
 
 export const Root = () => {
   return (
@@ -15,13 +13,6 @@ export const Root = () => {
         to Enhance Your Favorites Collection.
       </p>
       <Button className="w-[300px]">Start Searching</Button>
-      <div className="flex items-center justify-between gap-x-2 mt-4">
-        <SearchBar />
-        <div className="flex gap-x-2">
-          <Select options={["5", "10", "15"]} name="Results" />
-          <Select options={["Asc", "Desc"]} name="Sort" />
-        </div>
-      </div>
     </div>
   );
 };
