aa6906775ab8402d2ed6cb08f46373707ea76074
Wiktor Filip <karektv@gmail.com>
2024-04-02 17:49:32 +0200

    error page improvement

--- src/components/ErrorElement.tsx
+++ src/components/ErrorElement.tsx
@@ -1,4 +1,8 @@
-import { useNavigate, useRouteError } from "react-router-dom";
+import {
+  isRouteErrorResponse,
+  useNavigate,
+  useRouteError,
+} from "react-router-dom";
 import { Button } from "./Button";
 import { LinkButton } from "./LinkButton";
 
@@ -11,13 +15,22 @@ export const ErrorElement = () => {
       <h1 className="font-extrabold text-5xl">
         Oops! We got lost in the digital wilderness... 😵
       </h1>
-
       <p className="py-8 flex flex-col gap-y-2">
-        {error instanceof Error && (
-          <span className="text-grey-dark">{error.message}</span>
+        {isRouteErrorResponse(error) && error.status === 404 && (
+          <>
+            <span className="text-grey-dark">
+              {`${error.status} ${error.statusText}`}
+            </span>
+            The requested URL could not be located on our server. Please check
+            the web address for any typos or try a different page.
+          </>
         )}
-        An unexpected error occurred. you can try refreshing the page or go back
-        and try again.
+        {error instanceof Error && (
+          <>
+            An unexpected error occurred. you can try refreshing the page or go
+            back and try again.
+          </>
+        )}
       </p>
       <div className="flex gap-x-2 justify-center">
         <Button onClick={() => navigate(-1)}>Go back</Button>
